cca55d3ff8ae588639dd74df275b55a5e5e510db
김강년 <64246800+ekek54@users.noreply.github.com>
2024-05-05 00:54:23 +0900

    feat: User Builder Getter 적용

--- src/main/java/com/example/nyeon/auth/user/User.java
+++ src/main/java/com/example/nyeon/auth/user/User.java
@@ -8,9 +8,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@Getter
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 public class User {
@@ -34,4 +37,11 @@ public class User {
     @CreatedDate
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
+
+    @Builder
+    private User(String name, String email, String loginProvider) {
+        this.name = name;
+        this.email = email;
+        this.loginProvider = loginProvider;
+    }
 }
