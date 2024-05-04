3320360f36e8db18e401268d51cf229ec8ec407c
김강년 <64246800+ekek54@users.noreply.github.com>
2024-05-04 22:42:05 +0900

    chore: init repository

---
+++ src/test/java/com/example/nyeon/auth/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nyeon.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
