ea9b35a438d693b98dcd831301d795c92e1795ca
김강년 <64246800+ekek54@users.noreply.github.com>
2024-05-06 22:16:44 +0900

    feat: ResourceOwner jwt 발급 및 인증
    - jwt 발급 책임 위임
      - AuthenticationSuccessHandler -> SecurityContextRepository

--- src/main/java/com/example/nyeon/auth/config/SocialLoginConfig.java
+++ src/main/java/com/example/nyeon/auth/config/SocialLoginConfig.java
@@ -1,7 +1,9 @@
 package com.example.nyeon.auth.config;
 
 import com.example.nyeon.auth.security.CustomAuthenticationSuccessHandler;
+import com.example.nyeon.auth.security.JWTCookieSecurityContextRepository;
 import com.example.nyeon.auth.security.StatelessOAuth2AuthorizationRequestRepository;
+import com.example.nyeon.auth.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +11,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.security.web.savedrequest.CookieRequestCache;
 import org.springframework.security.web.savedrequest.RequestCache;
 
@@ -21,26 +25,33 @@ public class SocialLoginConfig {
     @Autowired
     private JwtEncoder jwtEncoder;
 
+    @Autowired
+    private JwtDecoder jwtDecoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Bean
     public SecurityFilterChain socailLoginSecurityFilterChain(HttpSecurity http)
             throws Exception {
+        //Formatter:off
         http
-            .authorizeHttpRequests((authorize) -> authorize
-                .anyRequest().authenticated()
-            )
-            .oauth2Login(oauth -> oauth
-                .successHandler(authenticationSuccessHandler())
-                .authorizationEndpoint(authorization -> authorization
-                    .authorizationRequestRepository(authorizationRequestRepository()))
-            )
-            .sessionManagement(session -> session
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            )
-            .csrf(AbstractHttpConfigurer::disable)
-            .requestCache(requestCache -> requestCache
-                .requestCache(cookieRequestCache())
-            )
-        ;
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("favicon.ico", "robots.txt", "error").permitAll()
+                        .anyRequest().authenticated()
+                ).oauth2Login(oauth -> oauth
+                        .successHandler(authenticationSuccessHandler())
+                        .authorizationEndpoint(authorization -> authorization
+                                .authorizationRequestRepository(authorizationRequestRepository())
+                        )
+                ).sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).csrf(AbstractHttpConfigurer::disable)
+                .requestCache(requestCache -> requestCache
+                        .requestCache(cookieRequestCache())
+                ).securityContext(securityContext -> securityContext
+                        .securityContextRepository(securityContextRepository())
+                );
         return http.build();
     }
 
@@ -57,5 +68,10 @@ public class SocialLoginConfig {
     @Bean
     public RequestCache cookieRequestCache() {
         return new CookieRequestCache();
+    }
+
+    @Bean
+    public SecurityContextRepository securityContextRepository() {
+        return new JWTCookieSecurityContextRepository(jwtEncoder, jwtDecoder, userRepository);
     }
 }
---
+++ src/main/java/com/example/nyeon/auth/security/JWTCookieSecurityContextRepository.java
@@ -0,0 +1,134 @@
+package com.example.nyeon.auth.security;
+
+import com.example.nyeon.auth.exception.BadRequestException;
+import com.example.nyeon.auth.exception.UnAuthorizedException;
+import com.example.nyeon.auth.security.principal.OAuth2UserPrincipal;
+import com.example.nyeon.auth.user.User;
+import com.example.nyeon.auth.user.UserRepository;
+import com.example.nyeon.auth.util.CookieUtil;
+import com.example.nyeon.auth.util.EncryptionUtil;
+import com.example.nyeon.auth.util.SerializeUtil;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.nio.file.attribute.UserPrincipal;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.UUID;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.server.Cookie.SameSite;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.DeferredSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.oauth2.client.authentication.OAuth2LoginAuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.jwt.BadJwtException;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.web.context.HttpRequestResponseHolder;
+import org.springframework.security.web.context.SecurityContextRepository;
+
+
+public class JWTCookieSecurityContextRepository implements SecurityContextRepository {
+    private static final Duration CONTEXT_COOKIE_EXPIRY = Duration.ofHours(1);
+    private static final String CONTEXT_COOKIE_NAME = "ROC"; // Resource Owner Context
+    private final JwtEncoder jwtEncoder;
+    private final JwtDecoder jwtDecoder;
+    private final SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder
+            .getContextHolderStrategy();
+    private final SecretKey encryptionKey;
+
+    public JWTCookieSecurityContextRepository(JwtEncoder jwtEncoder, JwtDecoder jwtDecoder, UserRepository userRepository) {
+        this.jwtEncoder = jwtEncoder;
+        this.jwtDecoder = jwtDecoder;
+        this.encryptionKey = EncryptionUtil.generateKey();
+    }
+
+    @Override
+    @Deprecated
+    public SecurityContext loadContext(HttpRequestResponseHolder requestResponseHolder) {
+        HttpServletRequest request = requestResponseHolder.getRequest();
+        Jwt contextJwt = retrieveContextJWT(request);
+        SecurityContext context = securityContextHolderStrategy.createEmptyContext();
+        if (contextJwt == null) {
+            //Empty context
+            return context;
+        }
+        JwtAuthenticationToken authentication = new JwtAuthenticationToken(contextJwt);
+        context.setAuthentication(authentication);
+        return context;
+    }
+
+
+    @Override
+    public DeferredSecurityContext loadDeferredContext(HttpServletRequest request) {
+        return SecurityContextRepository.super.loadDeferredContext(request);
+    }
+
+    @Override
+    public void saveContext(SecurityContext context, HttpServletRequest request, HttpServletResponse response) {
+        if (context == null) {
+            removeCookie(request, response);
+            return;
+        }
+        Jwt jwt = buildJwt(context);
+        Cookie cookie = buildJwtCookie(jwt, request);
+        response.addCookie(cookie);
+    }
+
+    private Jwt buildJwt(SecurityContext context) {
+        OAuth2User principal = (OAuth2User) context.getAuthentication().getPrincipal();
+        String userUUID = principal.getName();
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .subject(userUUID)
+                .expiresAt(Instant.now().plus(CONTEXT_COOKIE_EXPIRY))
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims));
+    }
+
+    @Override
+    public boolean containsContext(HttpServletRequest request) {
+        return retrieveContextJWT(request) != null;
+    }
+
+    private Jwt retrieveContextJWT(HttpServletRequest request) {
+        return CookieUtil.retrieve(request.getCookies(), CONTEXT_COOKIE_NAME)
+                .map(jwtDecoder::decode).orElse(null);
+    }
+
+    private Cookie buildJwtCookie(Jwt jwt, HttpServletRequest request) {
+        return CookieUtil.cookieBuilder(request)
+                .name(CONTEXT_COOKIE_NAME)
+                .value(jwt.getTokenValue())
+                .maxAge(CONTEXT_COOKIE_EXPIRY)
+                .httpOnly(true)
+                .sameSite(SameSite.STRICT)
+                .build();
+    }
+
+    private void removeCookie(HttpServletRequest request, HttpServletResponse response) {
+        Cookie expiredCookie = buildExpiredCookie(request);
+        response.addCookie(expiredCookie);
+    }
+
+    private Cookie buildExpiredCookie(HttpServletRequest request) {
+        return CookieUtil.cookieBuilder(request)
+                .name(CONTEXT_COOKIE_NAME)
+                .value("")
+                .maxAge(Duration.ZERO)
+                .httpOnly(true)
+                .build();
+    }
+}
