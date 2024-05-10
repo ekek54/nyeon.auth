5f6995098fbdf8840abbbcc7daeeac899bcce559
김강년 <64246800+ekek54@users.noreply.github.com>
2024-05-10 18:02:00 +0900

    feat: Oidc Id 토큰 커스텀
    - email, name, provider 기본으로 제공

---
+++ src/main/java/com/example/nyeon/auth/authorization/OidcUserInfoService.java
@@ -0,0 +1,27 @@
+package com.example.nyeon.auth.authorization;
+
+import com.example.nyeon.auth.exception.UserNotFoundException;
+import com.example.nyeon.auth.user.User;
+import com.example.nyeon.auth.user.UserRepository;
+import java.util.UUID;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OidcUserInfoService {
+    private final UserRepository userRepository;
+
+    public OidcUserInfoService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public OidcUserInfo loadUser(String userUUID) {
+        User user = userRepository.findById(UUID.fromString(userUUID)).orElseThrow(UserNotFoundException::new);
+        return OidcUserInfo.builder()
+                .subject(user.getId().toString())
+                .name(user.getName())
+                .email(user.getEmail())
+                .claim("provider", user.getLoginProvider())
+                .build();
+    }
+}
